f62e21dcabd36ae27c9cf540e93a291ef644361a
ксюша 3 <tyurinaksen111a@gmail.com>
2026-04-03 09:14:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,46 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    uint64_t maxVal = num;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = num * 3 + 1;
+        }
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+    return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned len = 1;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = 3 * num + 1;
+        }
+        len++;
+    }
+    return len;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int* maxlen,
+    uint64_t lbound,
+    uint64_t rbound) {
+    unsigned int bestNum = lbound;
+    *maxlen = 0;
+    for (uint64_t i = lbound; i <=  rbound; i++) {
+        unsigned int len = collatzLen(i);
+        if (len > *maxlen) {
+            *maxlen = len;
+            bestNum = i;
+        }
+    }
+    return bestNum;
 }
